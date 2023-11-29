129f0dc57db4a1dadbd262a3607ab43717d27d30
BMustafa97 <bilalmustafa1797@gmail.com>
2023-11-29 21:10:53 +0000

    added in s3 compnent and module

---
+++ modules/s3/s3.tf
@@ -0,0 +1,14 @@
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.15.1"
+
+  bucket = var.name
+  force_destroy = true
+  intelligent_tiering = {}
+  logging = {}
+  object_ownership = "BucketOwnerEnforced"
+  tags = {
+    "TerraformManaged"                            = "true"
+  }
+}
+
